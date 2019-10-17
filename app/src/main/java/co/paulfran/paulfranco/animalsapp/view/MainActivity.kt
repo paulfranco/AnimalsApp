2045cabb0deab5a32157fb728a22a527eeeb719e
Paul Franco <paulfranco@me.com>
2019-10-16 21:09:52 -0700

    implemented navigate back button

--- app/src/main/java/co/paulfran/paulfranco/animalsapp/view/MainActivity.kt
+++ app/src/main/java/co/paulfran/paulfranco/animalsapp/view/MainActivity.kt
@@ -2,13 +2,28 @@ package co.paulfran.paulfranco.animalsapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import co.paulfran.paulfranco.animalsapp.R
 
 class MainActivity : AppCompatActivity() {
 
+    // Back Button step 1
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setTitle("Animals App")
+
+
+        // Back Button step 2
+        navController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    // Back Button step 3
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
